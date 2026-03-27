2c883390364b375a0330ab715fdead6520bfd776
Giulio Vimercati <153507806+gammaBeta99@users.noreply.github.com>
2026-03-27 23:12:18 +0100

    Update erone.py

--- erone.py
+++ erone.py
@@ -1,15 +1,19 @@
 '''
-Calcolo dell'area di un triangolo usando la formula di Erone.
+Calcolo dell'area di un triangolo usando la formula di Erone di Alessandria.
 Il programma controlla la terna inserita e ti fornisce infinite possibilità
 di errore: non calcola l'are fin quand la terna inserita non corrisponde 
 a un triangolo matematicamente esistente.
 '''
 
-from math import sqrt
+from math import sqrt 
+
+#primo inserimento dei lati 
 
 l_1 = float (input ("Inserisci il primo lato:"))
 l_2 = float (input ("Inserisci il secondo lato:"))
 l_3 = float (input ("Inserisci il terzo lato:")) 
+
+#contollo della terna con possibilità di riscatto
 
 while (not ((l_3 < l_1 + l_2) and (l_1 < l_2 + l_3 ) and (l_2 < l_1 + l_3))) :
     print ("Il triangolo non esiste")
@@ -20,7 +24,7 @@ while (not ((l_3 < l_1 + l_2) and (l_1 < l_2 + l_3 ) and (l_2 < l_1 + l_3))) :
 
 p = (l_1 + l_2 + l_3) / 2 #p è il semiperimetro
 
-area = sqrt (p * (p - l_1) * (p - l_2) * (p - l_3))
+area = sqrt (p * (p - l_1) * (p - l_2) * (p - l_3)) #area secondo la frmula di Erone
 
 print (area)
 
