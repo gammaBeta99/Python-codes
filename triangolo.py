ea251c8fa2ef1c1c754449bfb89f1f01f6af3e89
Giulio Vimercati <153507806+gammaBeta99@users.noreply.github.com>
2026-03-27 15:17:28 +0100

    Update triangolo.py
    This easy codes takes three real numbers as input and then it determines if the number can genrate a triangle so then it determines whether the triangle is acute-angled, rectangular-angled or obtuse-angled.

--- triangolo.py
+++ triangolo.py
@@ -15,7 +15,9 @@ print (lati)
 if (lati[0] < lati[1] + lati[2]) and (lati[1] < lati[0] + lati[2]) and ( lati[2] < lati[0] + lati [1]) :
     print ('il triangolo esiste') 
 else : sys.exit('il triangolo non esiste') 
-    
+
+#determinare il tipo di triangolo tra ottusangolo, acutangolo, rettangolo
+
 if (lati[0]**2 + lati[1]**2 < lati[2]**2)    : print ('triangolo ottusangolo')
 elif (lati[0]**2 + lati[1]**2 > lati[2]**2)  : print ('triangolo acutangolo') 
 else                                         : print ('triangolo rettangolo')   
