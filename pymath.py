314308f1ea38cd27ec62c1be2b5f56db76c30728
Giulio Vimercati <153507806+gammaBeta99@users.noreply.github.com>
2026-03-30 14:10:50 +0200

    Update pymath.py

--- pymath.py
+++ pymath.py
@@ -2,3 +2,19 @@ import random
 
 def rand_range (xMin, xMax) :
   return xMin + (xMax - xMin) * random.random() 
+
+"""
+funzione rudimentale per stimare pi greco
+basata sulla generazione di numeri pseudo-casuali 
+""" 
+def calc_pi (Ngen) :
+  Ninside = 0
+  x = rand_range (0., 1.) 
+  y = rand_range (0., 1.)
+  for i in range (Ngen) :
+    x = rand_range (0., 1.) 
+    y = rand_range (0., 1.)
+    if (x**2 + y**2 < 1) :
+      Ninside += 1
+
+return 4. * (Ninside / Ngen)
